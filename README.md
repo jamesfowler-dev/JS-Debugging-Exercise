8c7ee862b19a09e734287adaf0b64d705dd8e9d9
Mark Briscoe <mark@mcmw.co.uk>
2025-03-03 15:00:48 +0000

    add: bugs

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# JS-Debugging
+# Javascript Debugging Exercise
+
+## There are 10 errors in this project relating to Javascript.
+
+The purpose of the exercise is to get the game up and running and fully functional.
+
+Firstly, click on Fork above to copy this repo into your own GitHub account.
+
+![alt text](image.png)
+
+Give your repo a name and click "Create fork".
+
+![alt text](image-1.png)
+
+Now you can create your project from your own repo and solve the problems.
+
+[Create a VS Code project from an existing repo.](https://docs.google.com/presentation/d/1Epa-zivW7rYkcqnvaLvFglTply8-UTXiXGMH6ejLpaA/edit?usp=sharing)
--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,7 @@ let randomNumber = Math.floor(Math.random() * 100);
 let guessesLeft = 10;
 
 // Listen for the click event on the check button
-checkButton.addEventListener('click', buttonClicked);
+checkButton.addEventListener('click', btnClicked);
 
 // process the button clicked event
 function buttonClicked() {
@@ -23,13 +23,13 @@ function buttonClicked() {
         window.location.reload();
     }
     // Decrement the chance variable on every click
-    guessesLeft--;
+    guessesLeft++;
 
     // Get the value from the input field
     let inputValue = inputElement.value;
 
     // Check if the input value is equal to the random number
-    if (inputValue == randomNumber) {
+    if (inputValue = randomNumber) {
         // Update guessed number, disable input, check button text and color.
         messageElement.textContent = `Congratulations - ${inputValue} is correct!`;
         inputElement.disabled = true;
@@ -38,7 +38,7 @@ function buttonClicked() {
         messageElement.style.color = 'green';
 
         //Check if input value is > random number and within 1-99 range.
-    } else if (inputValue > randomNumber && inputValue < 100) {
+    } else if (inputValue > randomNumber && inputValue < 100) 
         // Update the guess text and remaining chances
         messageElement.textContent = 'Your guess is high';
         remainingGuesses.textContent = guessesLeft;
@@ -47,12 +47,12 @@ function buttonClicked() {
         //Check if input value is < random number and within 1-99 range.
     } else if (inputValue < randomNumber && inputValue > 0) {
         // Update the guessed number text and remaining chances
-        messageElement.textContent = 'Your guess is low';
+        messageElement.textContext = 'Your guess is low';
         remainingGuesses.textContent = guessesLeft;
         messageElement.style.color = '#333';
 
         // If the input value is not within the range of 1 to 99
-    } else {
+     else {
         // Update the guessed number text, color and remaining chances
         messageElement.textContent = 'Your number is invalid';
         remainingGuesses.textContent = guessesLeft;
@@ -60,10 +60,10 @@ function buttonClicked() {
     }
 
     // Check if the chance is zero
-    if (guessesLeft == 0) {
+    if (guesses == 0) {
         //Update check button, disable input, and clear input value.
         // Update guessed number text and color to indicate user loss.
-        checkButton.textContent = 'Replay';
+        checkButton.textContent = "Replay';
         inputElement.disabled = true;
         inputValue = '';
         messageElement.textContent = 'You lost the game';
@@ -77,4 +77,4 @@ function buttonClicked() {
     // Set the focus on input field
     inputElement.focus();
     inputElement.value = '';
-}
+
